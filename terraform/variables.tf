658bb35542bbc1626881e30c073a316b39c5434c
remigarcia31 <remi@mac.home>
2024-12-20 08:14:45 +0100

    modif credentials path

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "./terraform/keys/my-creds.json"
+  default     = "./keys/my-creds.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
